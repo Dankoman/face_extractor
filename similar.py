366591f7bee5b7029b73508fd2e665abb65d4d2d
marqs <markus.rohsto@gmail.com>
2025-08-08 14:19:32 +0200

    added

--- similar.py
+++ similar.py
@@ -2,7 +2,7 @@ import pickle, numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from collections import defaultdict
 
-data = pickle.load(open("arcface_work-ppic/embeddings_ppic_merged.pkl","rb"))
+data = pickle.load(open("arcface_work-ppic/embeddings_ppic.pkl","rb"))
 X = np.vstack(data["X"])              # (N, 512)
 y = np.array(data["y"])               # etiketter (mappnamn)
 
